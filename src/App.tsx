d425f527db228fc1a205bff02f7de8b384305b90
AlexanderGrigoryan <a.grigoryan@inbox.ru>
2025-02-01 16:17:37 +0400

    configured routing and some basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-function App() {
+import { BrowserRouter, Navigate, Route, Routes } from "react-router"
+import Home from "./pages/Home"
+import History from "./pages/History"
+import PageNotFound from "./pages/PageNotFound"
+import GlobalStyles from "./styles/GlobalStyles"
 
+function App() {
   return (
     <>
-     
+      <GlobalStyles/>
+      <BrowserRouter>
+      <Routes>
+        <Route index element={<Navigate replace to="home"/>}/>
+        <Route path="home" element={<Home/>}/>
+        <Route path="history" element={<History/>}/>
+        <Route path="*" element={<PageNotFound/>}/>
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
